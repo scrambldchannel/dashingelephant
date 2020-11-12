dc50a76f2d951343e8d499c8e54398579e892875
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2020-11-12 23:40:07 +0100

    cosmetic

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -3,8 +3,8 @@
     <header class="border-t-14 border-teal-700">
       <nav class="container mx-auto flex flex-wrap justify-between items-center py-8">
         <div>
-          <g-link v-if="theme === 'theme-light'" to="/"><g-image src="../../static/logo.png" class="w-40" alt="logo" /></g-link>
-          <g-link v-else to="/"><g-image src="../../static/logo.png" class="w-40" alt="logo" /></g-link>
+          <g-link v-if="theme === 'theme-light'" class="uppercase tracking-wide font-bold text-copy-primary hover:text-gray-600" to="/">Code and Cricket</g-link>
+          <g-link v-else  class="no-underline uppercase tracking-wide font-bold text-copy-primary hover:text-gray-600" to="/">Code and Cricket</g-link>
         </div>
         <div class="block lg:hidden">
           <button @click="toggle" class="flex items-center px-3 py-2 border rounded border-gray-500 hover:text-gray-600 hover:border-gray-600" data-cypress="hamburger">
