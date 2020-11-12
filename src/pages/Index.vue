76aa055e1d4058344e25f3b410ef25e3d5abbfea
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2020-11-12 23:40:40 +0100

    cosmetics

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,19 +1,20 @@
 <template>
   <Layout>
-    <div class="hero container-inner mx-auto flex flex-col sm:flex-row justify-between py-16">
-      <div class="text-4xl font-bold w-full sm:w-3/5 text-center sm:text-left">
-        <div class="leading-tight">Alexander Sutcliffe</div>
-        <div class="text-teal-700 leading-tight">Business Intelligence Specialist</div>
+    <div class="hero container-inner mx-auto flex flex-col sm:flex-row justify-between py-16"  >
+      <div class="font-bold w-full sm:w-3/5 text-center sm:text-left">
+        <div class="leading-tight text-4xl ">Alex Sutcliffe</div>
+        <div class="text-teal-700 leading-tight text-2xl ">Business Intelligence Specialist</div>
       </div>
       <div class="mt-8 sm:mt-0">
         <!-- change this image --> 
-        <g-image src="../../static/developer.svg" alt="hero" class="mx-auto sm:mx-0" />
+        <g-image src="../../static/code_review.svg" alt="hero" class="mx-auto sm:mx-0" />
       </div>
     </div> <!-- end hero -->
 
     <div class="overflow-x-hidden">
       <div class="projects container-inner mx-auto text-xl border-t border-gray-500 border-b py-16 mb-16 relative">
-        <h2 class="font-bold mb-6" id="projects">Technical Skills</h2>
+        
+        <h2 class="font-bold mb-6" id="projects">Key Technical Skills</h2>
 
 
         <ul class="text-lg sm:text-xl space-y-6">
@@ -39,12 +40,12 @@
             <div class="text-lg text-gray-600">A track record of delivering projects on time and within budget with an emphasis on stakeholder communication.</div>
           </li>
           <li class="checkmark">
-            <div>Agile</div>
-            <div class="text-lg text-gray-600">Good level of proficiency for automation, analysis and data pipeline creation. I have also contributed to projects such as TM1py and Apahe Airflow.</div>
+            <div>Agile/DevOPs</div>
+            <div class="text-lg text-gray-600">Comfortable working in Agile environments using small iterations and CI/CD to deliver increased value and transparency to stakeholders.</div>
           </li>
           <li class="checkmark">
             <div>Team Lead</div>
-            <div class="text-lg text-gray-600">Extremely comfortable writing SQL for ad hoc analysis and ETL purposes and possess a good understanding of the basis of relational database design.</div>
+            <div class="text-lg text-gray-600">Experience with leading small teams both colocated and remote.</div>
           </li>
 
 
@@ -59,7 +60,7 @@
 
         <div class="flex flex-col sm:flex-row justify-between items-center mb-16">
           <div><g-image src="../../static/avatar.jpg" alt="avatar" class="w-32 h-32 rounded-full mb-8 lg:mb-0" /></div>
-          <div class="flex-1 text-lg sm:text-xl ml-6">Experienced developer sharing my passion for all things techn as well as my other interests such as photography, cycling and cricket.</div>
+          <div class="flex-1 text-lg sm:text-xl ml-6">Berlin based developer sharing my passion for all things tech as well as my other interests such as photography, cycling and cricket.</div>
         </div>
 
       </div> <!-- end get-to-know me -->
@@ -73,7 +74,7 @@
           <p class="mb-12">Have a question or want to discuss an opportunity? Drop me a note and I'll get back to you.</p>
 
           <div class="text-lg sm:text-lg mb-16">
-            <form action="#" class="mb-12">
+            <form action="#" class="mb-12" data-netlify="true">
               <div class="flex flex-wrap mb-6 -mx-4">
                   <div class="w-full md:w-1/2 mb-6 md:mb-0 px-4">
                       <label class="block mb-2 text-copy-primary" for="name">
