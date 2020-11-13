acef4c9f0455e01384edcd6218cd9cbb6151614b
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2020-11-13 14:59:52 +0100

    forms still

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -74,7 +74,7 @@
           <p class="mb-12">Have a question or want to discuss an opportunity? Drop me a note and I'll get back to you.</p>
 
           <div class="text-lg sm:text-lg mb-16">
-            <form name="contact" action="#" class="mb-12" data-netlify="true">
+            <form name="contact" action="#" method="POST" class="mb-12" data-netlify="true">
               <div class="flex flex-wrap mb-6 -mx-4">
                   <div class="w-full md:w-1/2 mb-6 md:mb-0 px-4">
                       <label class="block mb-2 text-copy-primary" for="name">
