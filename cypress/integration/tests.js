d95e7fcaa3c60e7dd0a289a78ec7223be72c425f
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2020-11-13 14:08:21 +0100

    basic tests

--- cypress/integration/tests.js
+++ cypress/integration/tests.js
@@ -1,12 +1,12 @@
 describe('Loading Pages', () => {
   it('visits the homepage', () => {
     cy.visit('/')
-      .contains('Gridsome Portfolio Starter')
+      .contains('Sutcliffe')
   })
 
   it('visits the blog page', () => {
     cy.visit('/blog')
-      .contains('Introduction to Gridsome')
+      .contains('Adventures with pre-commit')
   })
 
   it('can click the blog', () => {
@@ -15,164 +15,109 @@ describe('Loading Pages', () => {
     cy.get('[data-cypress=blog]').click()
 
     cy.url().should('include', '/blog')
-    cy.contains('Introduction to Gridsome')
+    cy.contains('Adventures with pre-commit')
   })
 
   it('can visit a single blog post', () => {
-    cy.visit('/introduction-to-gridsome')
+    cy.visit('/adventures-with-pre-commit')
 
-    cy.url().should('include', '/introduction-to-gridsome')
-    cy.contains('Introduction to Gridsome')
+    cy.url().should('include', '/adventures-with-pre-commit')
+    cy.contains('Adventures with pre-commit')
   })
 
   it('can click a single blog post', () => {
     cy.visit('/blog')
 
-    cy.contains('Introduction to Gridsome').click()
+    cy.contains('Adventures with pre-commit').click()
 
-    cy.url().should('include', '/introduction-to-gridsome')
-    cy.contains('Introduction to Gridsome')
+    cy.url().should('include', '/adventures-with-pre-commit')
+    cy.contains('Adventures with pre-commit')
   })
 
-  it('visits the docs page', () => {
-    cy.visit('/docs')
-      .contains('Vue Components in Markdown')
+
+  describe('Page Scroll to Sections', () => {
+
+    it('can scroll to About', () => {
+      cy.visit('/')
+
+      cy.get('[data-cypress=about]').click()
+      cy.get('#about').should('be.visible')
+    })
+
+    it('can scroll to Contact', () => {
+      cy.visit('/')
+
+      cy.get('[data-cypress=contact]').click()
+      cy.get('#contact').should('be.visible')
+    })
   })
 
-  it('can click the docs page', () => {
-    cy.visit('/')
+  describe('Pagination', () => {
+    // skipping these until there is sufficient content
+    it.skip('the next button works', () => {
+      cy.visit('/blog')
 
-    cy.get('[data-cypress=docs]').click()
+      cy.get('[data-cypress=next').click()
+      cy.url().should('include', '/blog/2')
+      cy.contains('Vue vs React Comparison')
+    })
 
-    cy.url().should('include', '/docs')
-    cy.contains('Vue Components in Markdown')
+    it.skip('the prev button works', () => {
+      cy.visit('/blog/2')
+
+      cy.get('[data-cypress=prev').click()
+      cy.url().should('include', '/blog')
+      cy.contains('Introduction to Gridsome')
+    })
   })
 
-  it('can visit a single doc post', () => {
-    cy.visit('/docs/vue-components-in-markdown')
+  describe('Tags for posts', () => {
+    it('can visit tags page', () => {
+      cy.visit('/tag/python')
 
-    cy.url().should('include', '/docs/vue-components-in-markdown')
-    cy.contains('Vue Components in Markdown')
+      cy.contains('Tag: python')
+      cy.contains('Adventures with pre-commit')
+      cy.contains('Creating a TI process from a .pro file using TM1py')
+    })
+
+    it('can click on tags from blog post page', () => {
+      cy.visit('/adventures-with-pre-commit')
+
+      cy.contains('python').click()
+      cy.url().should('include', '/tag/python')
+      cy.contains('Tag: python')
+      cy.contains('Adventures with pre-commit')
+    })
   })
 
-  it('can click a single doc post', () => {
-    cy.visit('/docs')
+  describe('Theme Switcher', () => {
+    it('can change to dark theme', () => {
+      cy.visit('/')
 
-    cy.contains('Vue Components in Markdown').click()
+      cy.get('[data-cypress=switchTheme]').click({ force: true })
+      cy.get('.content-wrapper').should('have.class', 'theme-dark')
+    })
 
-    cy.url().should('include', '/docs/vue-components-in-markdown')
-    cy.contains('Vue Components in Markdown')
+    it('can change to light theme', () => {
+      cy.visit('/')
+
+      cy.get('[data-cypress=switchTheme]').click({ force: true })
+      cy.get('[data-cypress=switchTheme]').click({ force: true })
+      cy.get('.content-wrapper').should('have.class', 'theme-light')
+    })
   })
+
+  describe('Search with Vue Fuse', () => {
+    it('can search for blog posts', () => {
+      cy.visit('/')
+
+      cy.get('[data-cypress=search-results]').should('not.be.visible')
+      cy.get('[data-cypress=search]').type('Adventures')
+      cy.get('[data-cypress=search-results]').should('be.visible')
+      cy.contains('Adventures with pre-commit')
+    })
+
+  })
+
 })
 
-describe('Page Scroll to Sections', () => {
-  it('can scroll to Projects', () => {
-    cy.visit('/')
-
-    cy.get('[data-cypress=projects]').click()
-    cy.get('#projects').should('be.visible')
-  })
-
-  it('can scroll to About', () => {
-    cy.visit('/')
-
-    cy.get('[data-cypress=about]').click()
-    cy.get('#about').should('be.visible')
-  })
-
-  it('can scroll to Contact', () => {
-    cy.visit('/')
-
-    cy.get('[data-cypress=contact]').click()
-    cy.get('#contact').should('be.visible')
-  })
-})
-
-describe('Pagination', () => {
-  it('the next button works', () => {
-    cy.visit('/blog')
-
-    cy.get('[data-cypress=next').click()
-    cy.url().should('include', '/blog/2')
-    cy.contains('Vue vs React Comparison')
-  })
-
-  it('the prev button works', () => {
-    cy.visit('/blog/2')
-
-    cy.get('[data-cypress=prev').click()
-    cy.url().should('include', '/blog')
-    cy.contains('Introduction to Gridsome')
-  })
-})
-
-describe('Tags for posts', () => {
-  it('can visit tags page', () => {
-    cy.visit('/tag/frontend')
-
-    cy.contains('Tag: frontend')
-    cy.contains('Introduction to Gridsome')
-    cy.contains('How to Get Better At Coding')
-    cy.contains('Top 5 Static Site Generators in Vue')
-  })
-
-  it('can click on tags from blog post page', () => {
-    cy.visit('/introduction-to-gridsome')
-
-    cy.contains('frontend').click()
-    cy.url().should('include', '/tag/frontend')
-    cy.contains('Tag: frontend')
-    cy.contains('Introduction to Gridsome')
-  })
-})
-
-describe('Theme Switcher', () => {
-  it('can change to dark theme', () => {
-    cy.visit('/')
-
-    cy.get('[data-cypress=switchTheme]').click({force: true})
-    cy.get('.content-wrapper').should('have.class', 'theme-dark')
-  })
-
-  it('can change to light theme', () => {
-    cy.visit('/')
-
-    cy.get('[data-cypress=switchTheme]').click({force: true})
-    cy.get('[data-cypress=switchTheme]').click({force: true})
-    cy.get('.content-wrapper').should('have.class', 'theme-light')
-  })
-})
-
-describe('Search with Vue Fuse', () => {
-  it('can search for blog posts', () => {
-    cy.visit('/')
-
-    cy.get('[data-cypress=search-results]').should('not.be.visible')
-    cy.get('[data-cypress=search]').type('Introduction')
-    cy.get('[data-cypress=search-results]').should('be.visible')
-    cy.contains('Introduction to Gridsome')
-    cy.contains('Gridsome is a Vue.js-powered')
-  })
-
-  it('can search for docs', () => {
-    cy.visit('/')
-
-    cy.get('[data-cypress=search-results]').should('not.be.visible')
-    cy.get('[data-cypress=search]').type('mark')
-    cy.get('[data-cypress=search-results]').should('be.visible')
-    cy.contains('Vue Components in Markdown')
-  })
-})
-
-// Doesn't work all the time, so I'm skipping it until I figure it out
-describe.skip('Mobile View', {
-  viewportWidth: 400,
-}, () => {
-  it('shows the hamburger menu', () => {
-    cy.visit('/')
-
-    cy.get('[data-cypress=menu]').should('not.be.visible')
-    cy.get('[data-cypress=hamburger]').should('be.visible').click()
-    cy.get('[data-cypress=menu]').should('be.visible')
-  })
-})
