7c11f7ecdc1de71adc5d366574208312bde9bfef
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2020-11-15 16:22:20 +0100

    add keywords

--- src/main.js
+++ src/main.js
@@ -22,7 +22,7 @@ export default function (Vue, { router, head, isClient }) {
 
   head.meta.push({
     name: 'keywords',
-    content: 'Gridsome,Vue,Tailwind,Tailwind CSS,JavaScript,HTML,CSS,Vue.js,VueJS'
+    content: 'Gridsome,Vue,TM1,Cognos,Planning Analytics, BI, SQL, Python,Julia, Berlin, Developer, Blog, Portfolio, Vue.js,VueJS'
   })
 
   head.meta.push({
