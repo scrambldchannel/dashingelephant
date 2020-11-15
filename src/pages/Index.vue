153a91d7b1722eef04e6bd2cc9006a4b6b12f7cd
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2020-11-15 14:11:00 +0100

    revert

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -210,7 +210,7 @@ export default {
         .join("&");
     },
     handleSubmit(e) {
-      fetch("/success", {
+      fetch("/", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: this.encode({
