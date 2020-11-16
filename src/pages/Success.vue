5950f702f43b5e87b7607b78cb515429de08608d
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2020-11-16 05:09:13 +0100

    tweak content

--- src/pages/Success.vue
+++ src/pages/Success.vue
@@ -2,17 +2,18 @@
   <Layout>
     <div class="container-inner mx-auto py-16">
       <h2 class="text-4xl font-bold mb-16">Thanks for getting in touch!</h2>
-      <p>I will review your mail and will get back to you soon. I looking forward to speaking to you. </p>
+      <p>
+        I will review your mail and will get back to you soon. Thamks for taking
+        an interest.
+      </p>
     </div>
-
   </Layout>
 </template>
 
 <script>
 export default {
   metaInfo: {
-    title: 'Thanks for getting in touch'
-  }
-}
+    title: "Thanks for getting in touch",
+  },
+};
 </script>
-
