0cd169756fe42a7681c18fcb4f1466321713c7b9
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2020-11-16 01:05:52 +0100

    a bit more text on the success page

--- src/pages/Success.vue
+++ src/pages/Success.vue
@@ -2,6 +2,7 @@
   <Layout>
     <div class="container-inner mx-auto py-16">
       <h2 class="text-4xl font-bold mb-16">Thanks for getting in touch!</h2>
+      <p>I will review your mail and will get back to you soon. I looking forward to speaking to you. </p>
     </div>
 
   </Layout>
