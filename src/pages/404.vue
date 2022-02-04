2fce486fdcc3bcbf290b61759a2f4a02e4a5060a
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2022-02-04 18:00:43 +0100

    feat: dicking around

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,7 +1,9 @@
 <template>
   <Layout>
     <div class="container-inner mx-auto py-16">
-      <h2 class="text-4xl font-bold mb-16">Page Not Found</h2>
+      <h2 class="text-4xl font-bold mb-16">
+        These are definitely not the droids you're looking for...
+      </h2>
     </div>
   </Layout>
 </template>
